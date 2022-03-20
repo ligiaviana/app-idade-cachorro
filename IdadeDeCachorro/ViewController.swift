5b48ef947197d90ed1d63a0d0388e751a3082985
ligiaviana <ligia.viana.azevedo@gmail.com>
2022-03-20 12:17:08 -0300

    Primeiro App, idade de cachorro.

---
+++ IdadeDeCachorro/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  IdadeDeCachorro
+//
+//  Created by Lígia Viana Azevedo on 11/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var legendaResultado: UILabel!
+    
+    @IBOutlet weak var campoIdadeCachorro: UITextField!
+    
+    @IBAction func descobrirIdade(_ sender: Any) {
+        
+        let idade = Int(campoIdadeCachorro.text!)! * 7
+        legendaResultado.text = "A idade do cachorro é:" + String(idade)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
